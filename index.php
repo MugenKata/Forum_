3b6aadd32373567aff4980ff1bc9b333652570b5
MugenKata <MugenKata@users.noreply.github.com>
2020-10-23 16:14:08 +0200

    Update index.php

--- index.php
+++ index.php
@@ -78,3 +78,4 @@
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script>window.jQuery || document.write('<script src="../assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
 </html>
+.
