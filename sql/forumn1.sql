570d43d81d575ff687fc3a4d2363548d4c30c569
Delacroix-75020 <57764513+Delacroix-75020@users.noreply.github.com>
2020-10-26 19:46:29 +0100

    SQL update

--- sql/forumn1.sql
+++ sql/forumn1.sql
@@ -1,22 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.9.1
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1
--- Généré le :  Dim 25 oct. 2020 à 03:05
--- Version du serveur :  10.4.8-MariaDB
--- Version de PHP :  7.3.11
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 Drop database if exists forumn1;
 Create database forumn1;
 
@@ -32,8 +14,9 @@ use forumn1;
 --
 
 CREATE TABLE `categories` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(20)
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -51,10 +34,11 @@ INSERT INTO `categories` (`id`, `name`) VALUES
 --
 
 CREATE TABLE `membres` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `pseudo` varchar(21),
   `email` varchar(100),
   `mdp` varchar(21)
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -71,11 +55,12 @@ INSERT INTO `membres` (`id`, `pseudo`, `email`, `mdp`) VALUES
 --
 
 CREATE TABLE `postsujet` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `propri` int(11),
   `contenu` text,
   `date` datetime,
   `sujet` varchar(60)
+   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -92,9 +77,10 @@ INSERT INTO `postsujet` (`id`, `propri`, `contenu`, `date`, `sujet`) VALUES
 --
 
 CREATE TABLE `sujet` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(60),
   `categorie` varchar(20)
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -105,62 +91,3 @@ INSERT INTO `sujet` (`id`, `name`, `categorie`) VALUES
 (1, 'testons', 'jeux');
 
 --
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `categories`
---
-ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `membres`
---
-ALTER TABLE `membres`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `postsujet`
---
-ALTER TABLE `postsujet`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `sujet`
---
-ALTER TABLE `sujet`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `categories`
---
-ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT pour la table `membres`
---
-ALTER TABLE `membres`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT pour la table `postsujet`
---
-ALTER TABLE `postsujet`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT pour la table `sujet`
---
-ALTER TABLE `sujet`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
